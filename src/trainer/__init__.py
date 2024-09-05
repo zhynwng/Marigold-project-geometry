1a433c70af6ca55b81297729d2543fa28ebc67a6
Tracy Zhu <61514951+Tracyyy-Zhu@users.noreply.github.com>
2024-09-05 01:32:52 -0500

    Update __init__.py

--- src/trainer/__init__.py
+++ src/trainer/__init__.py
@@ -3,11 +3,15 @@
 
 from .marigold_trainer import MarigoldTrainer
 from .finetune_trainer import FinetuneTrainer
+from .marigold_trainer_predict_image import MarigoldTrainerImage
+from .marigold_trainer_sdxl import MarigoldTrainerImageSDXL
 
 
 trainer_cls_name_dict = {
     "MarigoldTrainer": MarigoldTrainer,
     "FinetuneTrainer": FinetuneTrainer,
+    "MarigoldTrainerImage": MarigoldTrainerImage,
+    "MarigoldTrainerImageSDXL": MarigoldTrainerImageSDXL,
 }
 
 
