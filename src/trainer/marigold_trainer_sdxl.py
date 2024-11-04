9592f40d5706e01cf3a1f1b9526456b4716e24f2
Alex Wang <zhiyanw@beehive.ttic.edu>
2024-11-03 21:30:44 -0600

    guidance added

--- src/trainer/marigold_trainer_sdxl.py
+++ src/trainer/marigold_trainer_sdxl.py
@@ -97,7 +97,13 @@ class SDXLTrainer:
         self.model.vae.requires_grad_(False)
         self.model.text_encoder.requires_grad_(False)
         self.model.text_encoder_2.requires_grad_(False)
-        self.model.unet.requires_grad_(True)
+        self.model.unet.requires_grad_(False)
+
+        # only train first two layers of the unet
+        for param in self.model.unet.conv_in.parameters():
+            param.requires_grad = True
+
+    
         
         # Optimizer !should be defined after input layer is adapted
         lr = self.cfg.lr
